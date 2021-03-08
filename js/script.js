79eda15137800d030ed4c3a1069933376d3a8e5e
Rinat <64750426+rinat-lucky@users.noreply.github.com>
2021-03-09 00:15:45 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const closeMenu = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeMenu.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+const counters = document.querySelectorAll('.skills__ratings-counter');
+const lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
